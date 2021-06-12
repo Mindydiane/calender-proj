b48fe758a70ddfb203261c19d8a6eb8c9ca1770c
unknown <mindydiane@gmail.com>
2021-06-12 15:52:44 -0500

    bootstrap grid with time done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-/**
- * 
- * @param {Date} dateObject 
- */
 
 //displays date and time
 const dt = new Date();
 document.getElementById('date').innerHTML=dt;
 
+var buttons = document.querySelectorAll('.btn')
 
+function saveclk(event) {
+
+    
+   console.log(event.target.parentNode.parentNode.id);
+} 
+
+buttons.forEach((button) => {
+    console.log(button)
+    button.addEventListener('click', saveclk)
+});
 
 
 
