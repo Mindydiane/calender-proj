37a87ed4c4f7516da4bb1791e9808695b8098b3d
unknown <mindydiane@gmail.com>
2021-06-11 20:09:30 -0500

    added current date and time in header section

--- js/script.js
+++ js/script.js
@@ -2,20 +2,12 @@
  * 
  * @param {Date} dateObject 
  */
-function formatDate(dateObject) {
-    const parts = {
-        month: dateObject.getMonth() +1,
-        date: dateObject.getDate(),
-        year: dateObject.getFullYear(),
-        hour: (dateObject.getHours() % 12) || 12,
-        minute: dateObject.getMinutes().toString().padStart(2, "0"),
-        amOrPm: dateObject.getHours() < 12 ? "AM" : "PM"
-    };
 
-    return `${parts.month}/${parts.date}/${parts.year} ${parts.hour}:${parts.minute} ${parts.amOrPm}`;
-}
+//displays date and time
+const dt = new Date();
+document.getElementById('date').innerHTML=dt;
 
-const myDate = new Date();
-const myDateFormatted = formatDate(myDate);
 
-console.log(myDateFormatted);
+
+
+
