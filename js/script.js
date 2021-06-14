6e3f9fb99af717c66d01bc1258cb1feda3836255
unknown <mindydiane@gmail.com>
2021-06-13 19:49:53 -0500

    added jquery to script

--- js/script.js
+++ js/script.js
@@ -1,20 +1,69 @@
 
-//displays date and time
-const dt = new Date();
-document.getElementById('date').innerHTML=dt;
+//display date and time
+var rtNow = moment().format("ddd MMMM Do, YYYY; hh:mm:ss a");
+document.querySelector('#date').innerHTML=rtNow;
 
-var buttons = document.querySelectorAll('.btn')
+// var area = document.querySelector('#row');
+// for(let i = 0; i < rows.length; i++) {
+//   if (time === rtNow)
 
-function saveclk(event) {
+// }
+if (time === currentDate.format("H")) {
+  $("#taskEntry" + time)
+  //addClass
+  .addClass("present");
+  console.log("present");
+  //make new class
+} else if (time > currentDate.format("H"))  {
+  $("#taskEntry" + time)
+  
+}Else
+  $("#taskEntry" + time).addClass("past");
+  console.log("past");
 
+
+
+// textarea item 
+var itemlt = document.querySelector('.item')
+
+//button
+var btn = document.querySelector('.btn');
+
+var ni = document.querySelector('#nine');
+
+function colblk() {
+  if (rtNow > moment().hours())
+  $("textarea").addClass("past")
+  
+console.log(colblk);
+  
+}
+
+
+
+
+// function saveclk(event) { 
+//     itemlt = ' '; 
+//     document.getElementById("btn").addEventListener("click", saveclk)  
+//    console.log(event.target.parentNode.parentNode.id);
+// } 
+
+// buttons.forEach((button) => {
+
+//     // console.log(button)
+//     button.addEventListener('click', saveclk)
     
-   console.log(event.target.parentNode.parentNode.id);
-} 
+// });
+// localStorage.setItem(' ');
+// localStorage.clear();
+localStorage.setItem(btn, itemlt);
 
-buttons.forEach((button) => {
-    console.log(button)
-    button.addEventListener('click', saveclk)
-});
+// localStorage.setItem(textName, textValue);
 
-
+// audit task due dates every 30 minutes
+setInterval(function() {
+    $(".row .item").each(function() {
+      auditTask($(this));
+    });
+  }, 1800000);
 
